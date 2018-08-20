d82797e3f230e24d9616d0e5a4b90c90b68501b7
bwegh <baswegh@gmx.de>
2018-08-20 19:08:09 +0200

    use new stats interface

--- src/ctg_stats.erl
+++ src/ctg_stats.erl
@@ -9,7 +9,7 @@ add(Type, Duration) ->
     maybe_add(enabled(), Type, Duration).
 
 maybe_add(true, Type, Duration) ->
-    cts_messages:add(Type, Duration/1000.00);
+    ct_stats:add_message(Type, Duration/1000.00);
 maybe_add(_, _Trype, _Duration) ->
     ok.
 
@@ -20,6 +20,6 @@ update() ->
     maybe_update(enabled()).
 
 maybe_update(true) ->
-    cts_messages:update();
+    ct_stats:update();
 maybe_update(_) ->
     ok.
